e27662099b515c2ab52d65de90aec0e2caaff439
cakejune <tyq444@gmail.com>
2022-08-12 15:00:45 -0400

    commit tree d482bc88496eb7a55d8c8ff1cd0687501eeba5bd

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+*/
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+function writeCards(names, occasion) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`); 
+    debugger;
+    }
+    return messages;
+}
+
+function countDown(num) {
+    let i = num + 1;
+    while (i > 0) {
+        console.log(num--);
+        i = i - 1;
+    }
+}
